240f87a0ec717e241483f4858b584a7097357983
zenieldanaku <32693604Zd>
2020-10-04 01:04:10 -0300

    Trans Neptunian Objects + añadido un modulo en blanco con algunos datos sobre como formar   orbitas TNOs

---
+++ engine/equations/tnos.py
@@ -0,0 +1,24 @@
+# Classical Kuiper belt object
+# + low eccentricity
+# + orbits beyond last gas giant
+# + it s not in resonance with last gas giant.
+# + sometimes, low inclination
+# + planet-like orbit
+
+# Scattered disc objects
+# + orbits beyond last gas giant
+# + high eccentricity (up to 0.8)
+# + high inclination (up to 40°)
+# + periapsis > 30 au
+# + apoapsis up to 100 au
+
+# Sednoids
+# + inclination greater than 10° up to 25°
+# + high eccentricities > 0.8
+# + it s not in resonance with last gas giant.
+# + orbits beyond last gas giant
+
+# Detached objects
+# + orbits beyond last gas giant
+# + inclination > 25
+# + eccentricity 0.2 to 0.8
