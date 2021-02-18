71532e1ebb579c98bbf1f85aee25d2d2c7f7ac72
danielrossyamitrano <danielrossyamitrano@gmail.com>
2021-02-17 22:58:37 -0300

    El nuevo diagrama animado e interactivo de órbitas es funcional. + Muestra el ángulo de los planetas en su órbita para ubicarlos cuando   se encuentran por fuera de la pantalla. + Muestra también el rendimiento en FPS. - Simplificadas las funciones del módulo generate_orbits_diagram.py > Próximamente este paquete será intergrado al proyecto como una forma   de visualizar las órbitas.

--- animated_orbits_diagram.py
+++ animated_orbits_diagram.py
@@ -1,6 +1,6 @@
 from pygame import image, display, init, quit, event as events, K_ESCAPE, K_DOWN, K_LEFT, K_RIGHT, K_UP
-from pygame import KEYDOWN, QUIT, KEYUP, Surface, SRCALPHA, gfxdraw, time
-from generate_orbits_diagram import draw_orbits, orbitas, radiuses
+from pygame import KEYDOWN, QUIT, KEYUP, Surface, SRCALPHA, gfxdraw, font, time
+from generate_orbits_diagram import draw_orbits, abrir_json
 from pygame.sprite import Sprite, LayeredUpdates
 from math import sin, cos, radians
 from os import getcwd, path
@@ -10,103 +10,139 @@ from random import randint
 
 init()
 fps = time.Clock()
-fondo = display.set_mode((1200, 600))
-bg_rect = fondo.get_rect()
+
+orbitas = abrir_json(path.join(getcwd(), 'data.json'))
+radiuses = [item for sublist in orbitas.values() for item in sublist]
+radiuses.sort()
 
 bg_stars = image.load(path.join(getcwd(), 'data', 'estrellas.png'))
 w, h = bg_stars.get_size()
 rect = bg_stars.get_rect(topleft=(0, 0))
-draw_orbits(bg_stars, orbitas, radiuses)
-
-fondo.blit(bg_stars, (0, 0))
-
-planets = LayeredUpdates()
 
 
-class Planet(Sprite):
+class RotatingPlanet(Sprite):
     centerx = 0
-    centery = h//2
-    speed = 0
+    centery = h // 2
+    displaced = False
 
     def __init__(self, r):
         super().__init__()
         is_a_planet = False
-        radio = int(Decimal(r) * Decimal(100.0))
-        self.radius = radio
+        self._r = r
+        self.radius = int(Decimal(self._r) * Decimal(100.0))
         self.angle = randint(0, 360)
-        self.speed = radio
-        if orbitas['habitable zone'][0] < r < orbitas['habitable zone'][1]:  # main planet
+        if orbitas['habitable zone'][0] < self._r < orbitas['habitable zone'][1]:  # main planet
             self.image = Surface((6, 6), SRCALPHA)
             gfxdraw.filled_circle(self.image, 3, 3, 3, (255, 0, 255))
             is_a_planet = True
 
-        elif r in orbitas['inner']:  # terrestial planets
+        elif self._r in orbitas['inner']:  # terrestial planets
             self.image = Surface((6, 6), SRCALPHA)
             gfxdraw.filled_circle(self.image, 3, 3, 3, (255, 0, 0))
             is_a_planet = True
 
-        elif r in orbitas['outer']:  # gas giants
+        elif self._r in orbitas['outer']:  # gas giants
             self.image = Surface((22, 22), SRCALPHA)
             gfxdraw.filled_circle(self.image, 11, 11, 11, (0, 0, 255))
             is_a_planet = True
 
         if is_a_planet:
             width_2 = self.image.get_width() // 2
-            self.rect = self.image.get_rect(topleft=(radio - width_2, h // 2))
+            self.w2 = width_2
+            self.rect = self.image.get_rect(topleft=(self.radius - width_2, h // 2))
 
         self.is_a_planet = is_a_planet
 
-    @staticmethod
-    def set_xy(center, radius, angle, off_x=0, off_y=0):
-        x = round(off_x + center[0] + radius * cos(radians(angle)))
-        y = round(off_y + center[1] + radius * sin(radians(angle)))
-        return x, y
-
-    def update(self, delta_x=0, delta_y=0):
-        if 0 <= self.angle + 1 <= 360:
-            self.angle += (self.radius/360)
-        else:
-            self.angle = 0
-        self.centerx += delta_x
-        self.centery += delta_y
-        x, y = self.set_xy([self.centerx-1, self.centery], self.radius, self.angle, delta_x, delta_y)
-
+    def set_xy(self, off_x=0, off_y=0):
+        x = round(off_x + (self.centerx - self.w2) + self.radius * cos(radians(self.angle)))
+        y = round(off_y + self.centery + self.radius * sin(radians(self.angle)))
         self.rect.topleft = (x, y)
 
+    def displace(self, delta_x, delta_y):
+        self.centerx += delta_x
+        self.centery += delta_y
+        self.set_xy(delta_x, delta_y)
+        self.displaced = True
 
-for ridx in radiuses:
-    p = Planet(ridx)
-    if p.is_a_planet:
-        planets.add(p)
+    def update(self):
+        delta = (1 / (self._r / 360)) / 200
+        if 0 <= self.angle + delta < 360:
+            self.angle += delta  # velocidad angular, pero en realidad es un mamarracho.
+        else:
+            self.angle = -delta
 
-dx, dy = 0, 0
-while True:
-    fps.tick(30)
-    for e in events.get((KEYDOWN, KEYUP, QUIT)):
-        if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
-            quit()
-            exit()
+        self.set_xy()
 
-        elif e.type == KEYDOWN:
-            if e.key == K_UP:
-                dy -= 1
-            elif e.key == K_DOWN:
-                dy += 1
-            elif e.key == K_LEFT:
-                dx -= 3
-            elif e.key == K_RIGHT:
-                dx += 3
 
-        elif e.type == KEYUP:
-            if e.key == K_UP or e.key == K_DOWN:
-                dy = 0
-            elif e.key == K_LEFT or e.key == K_RIGHT:
-                dx = 0
+def topdown_loop():
+    blanco = 255, 255, 255
+    negro = 0, 0, 0
 
-    if rect.right + dx > 1200 and rect.left + dx <= 0 and rect.top + dy <= 0 and rect.bottom + dy > 600:
-        rect.move_ip(dx, dy)
-        planets.update(dx, dy)
-    fondo.blit(bg_stars, rect.topleft)
+    fondo = display.set_mode((1200, 600))
+    bg_rect = fondo.get_rect()
+    draw_orbits(bg_stars, radiuses, orbitas)
 
-    planets.draw(fondo)
-    display.update()
+    f1 = font.SysFont('Verdana', 12)
+    f2 = font.SysFont('Verdana', 16, bold=True)
+
+    text = f2.render('Top-Down View', 1, blanco, negro)
+    text_rect = text.get_rect()
+
+    planets = LayeredUpdates()
+    for ridx in radiuses:
+        p = RotatingPlanet(ridx)
+        if p.is_a_planet:
+            planets.add(p)
+
+    dx, dy = 0, 0
+    while True:
+        fps.tick(30)
+        for e in events.get((KEYDOWN, KEYUP, QUIT)):
+            if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
+                quit()
+                exit()
+
+            elif e.type == KEYDOWN:
+                if e.key == K_UP:
+                    dy += 3
+                elif e.key == K_DOWN:
+                    dy -= 3
+                elif e.key == K_LEFT:
+                    dx += 3
+                elif e.key == K_RIGHT:
+                    dx -= 3
+
+            elif e.type == KEYUP:
+                if e.key == K_UP or e.key == K_DOWN:
+                    dy = 0
+                elif e.key == K_LEFT or e.key == K_RIGHT:
+                    dx = 0
+
+        if rect.right + dx > 1200 and rect.left + dx <= 0 and rect.top + dy <= 0 and rect.bottom + dy > 600:
+            rect.move_ip(dx, dy)
+            for planet in planets:
+                # noinspection PyUnresolvedReferences
+                planet.displace(dx, dy)
+
+        fondo.blit(bg_stars, rect.topleft)
+        planets.update()
+        planets.draw(fondo)
+
+        for i, planet in enumerate(planets):
+            render = f1.render(str(round(planet.angle))+'°', 1, (255, 255, 255), (0, 0, 0))
+            if i % 2 == 0:
+                j = 11
+            else:
+                j = -11
+            renderrect = render.get_rect(left=planet.radius + rect.x + 3, centery=rect.centery + j)
+            fondo.fill((0, 0, 0), renderrect)
+            fondo.blit(render, (*renderrect.topleft, 8 * 3, 21))
+        fps_render = f1.render(str(round(fps.get_fps())), 1, blanco, negro)
+        fps_rect = fps_render.get_rect(topright=(bg_rect.right, 0))
+        fondo.blit(text, text_rect)
+        fondo.blit(fps_render, fps_rect)
+        display.update()
+
+
+if __name__ == '__main__':
+    topdown_loop()
--- generate_orbits_diagram.py
+++ generate_orbits_diagram.py
@@ -1,17 +1,7 @@
-from pygame import font, gfxdraw, Rect, PixelArray, Color  # , image, Surface, SRCALPHA
+from pygame import gfxdraw, Rect, PixelArray, Color
 from decimal import Decimal, getcontext
 from random import randint
 from json import load
-from os import getcwd, path
-from math import cos, sin, radians
-
-font.init()
-
-
-def set_xy(center, radius, angle: int):
-    x = round(center[0] + radius * cos(radians(angle)))
-    y = round(center[1] + radius * sin(radians(angle)))
-    return x, y
 
 
 def paint_stars(surface, i, f):
@@ -43,20 +33,13 @@ def paint_stars(surface, i, f):
     return surface
 
 
-def draw_orbits(fondo, orbits, radix):
+def draw_orbits(fondo, radix, orbits):
     getcontext().prec = 2
     surf_rect = fondo.get_rect()
 
     gfxdraw.filled_circle(fondo, *surf_rect.midleft, 20, (255, 255, 0))
     for r in radix:
         radio = int(Decimal(r) * Decimal(100.0))
-
-        # condition = False
-        # x, y = radio, surf_rect.centery
-        # while not condition:
-        #     a = randint(-90, 90)
-        #     x, y = set_xy(surf_rect.midleft, radio, a)
-        #     condition = surf_rect.collidepoint(x, y)
         rect = Rect(0, 0, radio, radio)
         rect.center = surf_rect.midleft
         if r in orbits['frost line']:  # frost line
@@ -67,37 +50,7 @@ def draw_orbits(fondo, orbits, radix):
             color = 125, 125, 125
         gfxdraw.ellipse(fondo, *surf_rect.midleft, radio, radio, color)
 
-    #     if orbits['habitable zone'][0] < r < orbits['habitable zone'][1]:  # main planet
-    #         gfxdraw.filled_circle(fondo, x, y, 3, (255, 0, 255))
-    #
-    #     elif r in orbits['inner']:  # terrestial planets
-    #         gfxdraw.filled_circle(fondo, x, y, 3, (255, 0, 0))
-    #
-    #     elif r in orbits['outer']:  # gas giants
-    #         gfxdraw.filled_circle(fondo, x, y, 11, (0, 0, 255))
-    #
-    # return fondo
-
 
 def abrir_json(archivo, encoding='utf-8'):
     with open(archivo, encoding=encoding) as file:
         return load(file)
-
-
-# if path.exists(path.join(getcwd(), 'data/estrellas.png')):
-#     surf = image.load(path.join(getcwd(), 'data/estrellas.png'))
-# else:
-#     surf = Surface((4500, 768), SRCALPHA)
-#     surf.fill((0, 0, 0, 255))
-#
-#     surf = paint_stars(surf, 0, 5000)
-#     image.save(surf, path.join(getcwd(), 'data/estrellas.png'))
-#
-orbitas = abrir_json(path.join(getcwd(), 'data.json'))
-
-radiuses = [item for sublist in orbitas.values() for item in sublist]
-radiuses.sort()
-#
-# surf = draw_orbits(surf, orbitas, radiuses)
-#
-# image.save(surf, path.join(getcwd(), 'data/orbitas.png'))
