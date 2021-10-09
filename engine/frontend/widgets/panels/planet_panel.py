1bfe699790ea3a2e26d5da343f0033d4563492a9
danielrossyamitrano <danielrossyamtrano@gmail.com>
2021-10-09 19:12:08 -0300

    Nuevo método BaseWidget.show_no_system_error() + Muestra el mensaje de error en los paneles que requieren un sistema   creado cuando éste no existe. * Corregido un crash cuando se pasaban los paneles vacíos.

--- engine/frontend/widgets/panels/planet_panel.py
+++ engine/frontend/widgets/panels/planet_panel.py
@@ -139,7 +139,7 @@ class PlanetPanel(BasePanel):
 
     def update(self):
         idx = Systems.get_current_idx()
-        if idx != self.last_idx and idx is not None:
+        if idx != self.last_idx and idx > 0:
             self.show_current(idx)
             self.last_idx = idx
 
