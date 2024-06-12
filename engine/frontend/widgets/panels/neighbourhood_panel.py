b9e62391fecb288e4b6e34b84dc1e8866ca7b857
danielrossyamitrano <danielrossyamitrano@gmail.com>
2024-06-12 14:55:35 -0300

    Faltó subir un archivo

--- engine/frontend/widgets/panels/neighbourhood_panel.py
+++ engine/frontend/widgets/panels/neighbourhood_panel.py
@@ -142,11 +142,19 @@ class NeighbourhoodPanel(BaseWidget):
             object_data.set_quantity(comp.title(), quantity)
             for each in range(quantity):
                 system_position = d[comp][each]
-                system_data = {'composition': comp, 'location': system_position, 'idx': i}
+                system_data = {'composition': comp, 'location': system_position, 'idx': i,
+                               'neighbourhood_id': object_data.id}
+                if self.analized is not None and each < len(self.analized[comp.title()]):
+                    system_id = self.analized[comp.title()][each]
+                    system_data.update({'id': system_id})
+
                 system_object = ProtoSystem(system_data)
-                Universe.add_astro_obj(system_object)
+                object_data.add_proto_system(system_object)
 
         return button
+
+    def set_analyzed(self, pairs):
+        self.analized = pairs
 
     def get_values(self, location, radius):
         density = self.galaxy.current.get_density_at_location(location)
@@ -217,7 +225,7 @@ class GalaxyType(BaseWidget):
         super().__init__(parent)
         self.current = None
         EventHandler.register(self.save_galaxy, 'Save')
-        EventHandler.register(self.load_galaxies, 'LoadData')
+        EventHandler.register(self.load_galaxies, 'LoadGalaxies')
         EventHandler.register(self.switch_current, 'SwitchGalaxy')
 
     def fill(self, data=None):
@@ -248,11 +256,10 @@ class GalaxyType(BaseWidget):
 
     def save_galaxy(self, event):
         widget = self.parent.properties.get_widgets_from_layer(1)[0]
-        data = {}
         if widget.value != '':
-            data["radius"] = widget.value
+            data = {"radius": widget.value, 'flagged': self.current.flagged}
 
-        EventHandler.trigger(event.tipo + 'Data', 'Galaxy', {'Galaxies': {self.current.id: data}})
+            EventHandler.trigger(event.tipo + 'Data', 'Galaxy', {'Galaxies': {self.current.id: data}})
 
     def load_galaxies(self, event):
         for key in event.data['Galaxies']:
@@ -274,7 +281,7 @@ class NeighbourhoodType(BaseWidget):
         self.characteristics = StellarNeighbourhood(self)
         self.title_font = self.crear_fuente(16, underline=True)
         EventHandler.register(self.save_neighbourhoods, 'Save')
-        EventHandler.register(self.load_neighbourhoods, 'LoadData')
+        EventHandler.register(self.load_neighbourhoods, 'LoadNeighbourhoods')
         self.values = {}
 
         f = self.crear_fuente(16, bold=True)
@@ -296,7 +303,7 @@ class NeighbourhoodType(BaseWidget):
             location_text.value = q(location_text.value, 'ly')
             location_valid = True
             location_text.editable = False
-            density_text.value = q(density)
+            density_text.value = q(density * 1000)
 
         if type(radius_text.value) in (str, float) and radius_text.value != '':
             self.characteristics.set_radius(float(radius_text.value))
@@ -329,6 +336,7 @@ class NeighbourhoodType(BaseWidget):
         EventHandler.trigger(event.tipo + 'Data', 'Neighbourhood', {"Neighbourhoods": macro_data})
 
     def load_neighbourhoods(self, event):
+        self.parent.set_analyzed(analyze_binaries(event.data))
         self.characteristics.set_galaxy()
         if len(event.data['Neighbourhoods']):
             for key in event.data['Neighbourhoods']:
@@ -429,15 +437,208 @@ class NeighbourhoodType(BaseWidget):
                 self.fill()
 
 
+class StellarNeighbourhood:
+    _o_stars = 0
+    _b_stars = 0
+    _a_stars = 0
+    _f_stars = 0
+    _g_stars = 0
+    _k_stars = 0
+    _m_stars = 0
+    _w_dwarfs = 0
+    _b_dwarfs = 0
+    _other = 0
+
+    _total_stars = 0
+    _total_systems = 0
+
+    _single = 0
+    _binary = 0
+    _triple = 0
+    _multiple = 0
+
+    radius = None
+
+    location = None
+    density = None
+
+    celestial_type = 'stellar bubble'
+
+    main_sequence_stars = 0
+
+    galaxy = None
+
+    def __init__(self, parent):
+        self.parent = parent
+
+    def set_galaxy(self):
+        self.galaxy = self.parent.parent.galaxy.current
+
+    def set_location(self, location, known_density=None):
+        self.location = location
+        if known_density is not None:
+            self.galaxy.record_density_at_location(location, known_density)
+            density = known_density
+        else:
+            density = self.galaxy.get_density_at_location(location)
+        if density is None:
+            self.density = uniform(0.003, 0.012)
+            self.galaxy.record_density_at_location(location, self.density)
+        else:
+            self.density = density
+        return self.density
+
+    def set_radius(self, radius):
+        assert radius < 500, 'Stellar Neighbourhood will pop out of the galatic disk'
+        self.radius = q(radius, 'ly')
+        self._calculate(self.density, self.radius.m)
+
+    def recalculate(self, density, radius):
+        self._calculate(float(density), float(radius))
+
+    def _calculate(self, density, radius):
+        stellar_factor = density * ((4 / 3) * pi * radius ** 3)
+
+        self._o_stars = round(stellar_factor * 0.9 * 0.0000003, 0)
+        self._b_stars = round(stellar_factor * 0.9 * 0.0013, 0)
+        self._a_stars = round(stellar_factor * 0.9 * 0.006, 0)
+        self._f_stars = round(stellar_factor * 0.9 * 0.03, 0)
+        self._g_stars = round(stellar_factor * 0.9 * 0.076, 0)
+        self._k_stars = round(stellar_factor * 0.9 * 0.121, 0)
+        self._m_stars = round(stellar_factor * 0.9 * 0.7645, 0)
+        self._w_dwarfs = round(stellar_factor * 0.09, 0)
+        self._b_dwarfs = round(stellar_factor / 2.5, 0)
+        self._other = floor(stellar_factor * 0.01)
+
+        self._total_stars = sum([self._o_stars, self._b_stars, self._a_stars, self._f_stars, self._g_stars,
+                                 self._k_stars, self._m_stars, self._w_dwarfs, self._b_dwarfs, self._other])
+
+        self.main_sequence_stars = sum([self._o_stars, self._b_stars, self._a_stars, self._f_stars, self._g_stars,
+                                        self._k_stars, self._m_stars])
+
+        # The distribution of individual stars into systems only takes into account main sequence stars because
+        # the program doesn't allow for neither the creation of brown or white dwarves or black holes yet.
+        # this might change in future revisions.
+        self._binary = int(round(((self.main_sequence_stars / 1.58) * 0.33), 0))
+        self._triple = int(round(((self.main_sequence_stars / 1.58) * 0.08), 0))
+        self._multiple = int(round(((self.main_sequence_stars / 1.58) * 0.03), 0))
+        self._single = int(self.main_sequence_stars - ((self._binary * 2) + (self._triple * 3) + (self._multiple * 4)))
+        # se agregan los cuerpos compactos porque por definición son sistemas simples.
+        self._single += int(self._w_dwarfs + self._b_dwarfs + self._other)
+
+        self._total_systems = int(sum([self._single, self._binary, self._triple, self._multiple]))
+
+        # this is because some of the binary pairs are part
+        self._binary += self._triple + (self._multiple * 2)
+        # of triple and cuadruple systems.
+
+        self.individual_stars = [{'class': 'O', 'idx': i} for i in range(int(self._o_stars))]
+        self.individual_stars += [{'class': 'B', 'idx': i} for i in range(int(self._b_stars))]
+        self.individual_stars += [{'class': 'A', 'idx': i} for i in range(int(self._a_stars))]
+        self.individual_stars += [{'class': 'F', 'idx': i} for i in range(int(self._f_stars))]
+        self.individual_stars += [{'class': 'G', 'idx': i} for i in range(int(self._g_stars))]
+        self.individual_stars += [{'class': 'K', 'idx': i} for i in range(int(self._k_stars))]
+        self.individual_stars += [{'class': 'M', 'idx': i} for i in range(int(self._m_stars))]
+
+    def stars(self, spectral_type: str = 'g') -> int:
+
+        types = 'o,b,a,f,g,k,m,wd,white,brown,bd,black,black hole'.split(',')
+        assert spectral_type in types, f'spectral_type "{spectral_type}" is unrecognizable.'
+
+        if spectral_type == 'o':
+            returned = self._o_stars
+        elif spectral_type == 'b':
+            returned = self._b_stars
+        elif spectral_type == 'a':
+            returned = self._a_stars
+        elif spectral_type == 'f':
+            returned = self._f_stars
+        elif spectral_type == 'g':
+            returned = self._g_stars
+        elif spectral_type == 'k':
+            returned = self._k_stars
+        elif spectral_type == 'm':
+            returned = self._m_stars
+        elif spectral_type in ('wd', 'white'):
+            returned = self._w_dwarfs
+        elif spectral_type in ('bd', 'brown'):
+            returned = self._b_dwarfs
+        else:
+            returned = self._other
+
+        return int(returned)
+
+    def systems(self, configuration: str = 'single') -> int:
+        if configuration == 'single':
+            return self._single
+        elif configuration == 'binary':
+            return self._binary
+        elif configuration == 'triple':
+            return self._triple
+        elif configuration == 'multiple':
+            return self._multiple
+        else:
+            raise ValueError('Configuration is invalid.')
+
+    def totals(self, kind: str = 'stars'):
+        if kind == 'stars':
+            return self._total_stars
+        elif kind == 'systems':
+            return self._total_systems
+        elif kind == 'main sequence':
+            return self.main_sequence_stars
+        else:
+            raise ValueError(f'Kind "{kind}" is unrecognizable.')
+
+    def system_positions(self, current_neighbourhood):
+        if current_neighbourhood.nei_seed is None:
+            seed = 1
+        else:
+            seed = current_neighbourhood.nei_seed
+
+        systems = ['Single'] * (self.systems('single')) + ['Binary'] * self.systems('binary')
+        systems += ['Triple'] * self.systems('triple') + ['Multiple'] * self.systems('multiple')
+
+        divisor = 2 ** 31 - 1
+        constant = 48271
+
+        initial_value = (constant * seed) % divisor
+        r_raw = initial_value
+        distances = []
+        for i, system in enumerate(systems):
+            if len(current_neighbourhood.pre_processed_system_positions[system]):
+                x, y, z = current_neighbourhood.pre_processed_system_positions[system].pop()
+            else:
+                p_raw = constant * r_raw % divisor
+                w_raw = constant * p_raw % divisor
+                r_raw = constant * w_raw % divisor
+
+                p_normal = p_raw / divisor
+                w_normal = w_raw / divisor
+                r_normal = r_raw / divisor
+
+                p = p_normal ** (1 / 3) * self.radius.m
+                w = w_normal * 2 * pi
+                r = acos(2 * r_normal - 1)
+
+                x = round(p * sin(r) * cos(w), 2)
+                y = round(p * sin(r) * sin(w), 2)
+                z = round(p * cos(r), 2)
+
+            distances.append({'configuration': systems[i], 'pos': [x, y, z]})
+
+        return distances
+
+
 class NeighbourhoodButton(Meta):
     enabled = True
 
-    def __init__(self, parent, nei_object, size=16):
+    def __init__(self, parent, nei_object):
         super().__init__(parent)
-        self.f1 = self.crear_fuente(size)
-        self.f2 = self.crear_fuente(size, bold=True)
+        self.f1 = self.crear_fuente(13)
+        self.f2 = self.crear_fuente(13, bold=True)
         self.object_data = nei_object
-        text = f"{turn_into_roman(nei_object.idx)}@{nei_object.location}"
+        text = f"{str(nei_object)}@{nei_object.location}"
         self.img_uns = self.f1.render(text, True, COLOR_TEXTO)
         self.img_sel = self.f2.render(text, True, COLOR_TEXTO)
 
